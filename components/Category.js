f69bfcd38fe1d5e1bde7aa29cb169f3bb5342d63
Kuldeep Ranjan <ranjankuldeep19@gmail.com>
2022-10-19 15:52:33 +0530

    Bottom Bar is added, Kuldeep

--- components/Category.js
+++ components/Category.js
@@ -1,110 +1,107 @@
 import {
-    View,
-    Text,
-    StyleSheet,
-    SafeAreaView,
-    TextInput,
-    TouchableOpacity,
-    Alert,
-    FlatList,
-    ScrollView,
-  } from "react-native";
-import CategoryData  from "../Data/CategoryData";
-import { useState, } from "react";
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import { useState } from "react";
+import React from "react";
 import { useLayoutEffect } from "react";
-// import data from "../Data/dummy_data";
-  function Category(){
-    const [data, setData] = useState([]);
-    const [count, setCount] = useState(0)
 
-    console.log(data)
-      useLayoutEffect(() => {
-        fetch('https://apiv2.orbiting.in/api/categories',{
-      method: 'GET',
+function Category() {
+  const [data, setData] = useState([]);
+  const [span, setSpan] = useState([]);
+
+  useLayoutEffect(() => {
+    fetch("https://apiv2.orbiting.in/api/categories", {
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-
-    }).then(res => res.json())
-      .then((res)=> {
+    })
+      .then((res) => res.json())
+      .then((res) => {
         const Arr = [];
-        const DATA = res.data
-        
+        const DATA = res.data;
 
-        DATA.forEach(item=> {
-          
-          
-            Arr.push({id:item.id, name: item.attributes.name, isSelected:false})})
-            
-            setData(Arr)
+        DATA.forEach((item) => {
+          Arr.push({
+            id: item.id,
+            name: item.attributes.name,
+            isSelected: false,
+          });
+        });
+
+        setData(Arr);
       })
-      .catch(console.error)
-      //  console.log(data.id, data.isSelected)
-      // console.log(data)
-    },[])
-    
-    
-    function selectionHandler(ind){
+      .catch(console.error);
+  }, []);
 
-      
-    let ga =  data.map((item, index)=> {
-      // console.log(index)
-       if (ind== index){
-        item.isSelected = !item.isSelected
-        if(item.isSelected == true){
-          setCount((count)=> count+1)
-        }
-        else if(item.isSelected == false){
-          setCount((count)=> count-1)
-        }
-       }
-       return item
-      })
-      setData(ga)
-      
-     console.log(count)
-    }
-
-    function renderComponent({item,index}){
-          
-          
-
-
-        return(
-            <TouchableOpacity onPress={()=> selectionHandler(index)} >
-                
-                <View   >
-
-                <View className="mt-2  mr-2 overflow-hidden  w-40 h-40 p-4-5 rounded-lg">
-                    <View className="  flex-1 bg-slate-500 text-center justify-center  " style={{backgroundColor: item.isSelected == true? '#ffff00': '#D3D3D3'}}>
-                    <Text className="text-center text-2xl font-bold " >{item.name}</Text>
-                    </View>
-                    
-                </View>
-                
-                </View>
-            </TouchableOpacity>
-        )
-    }
-
-
-
-    return(
-        <SafeAreaView className="mt-4 mx-1">
-            
-
-            <View className="mt-6 text-centre">
-                <Text className="font-bold text-center text-black text-3xl">Follow any three!</Text>
-                <Text className="font-bold text-center text-zinc text-base">Then we'll build a custom homefeed for you</Text>
-            </View>
-            {/* Render Component */}
-            <View >
-            <FlatList numColumns={2} data={data} renderItem={renderComponent} keyExtractor={(item)=> item.id} className="w-full h-f/5 p-3 flex-row flex-wrap"  />
-            </View>
-            
-            
-        </SafeAreaView>
-    )
+  function selectionHandler(ind) {
+    let ga = data.map((item, index) => {
+      if (ind === index) {
+        item.isSelected = !item.isSelected;
+      }
+      return item;
+    });
+    setData(ga);
+    const width = ga.filter((item) => item.isSelected === true);
+    setSpan(width);
   }
 
-  export default Category;
+  function renderComponent({ item, index }) {
+    return (
+      <TouchableOpacity onPress={() => selectionHandler(index)}>
+        <View>
+          <View className="mt-2  mr-2 overflow-hidden  w-40 h-40 p-4-5 rounded-lg">
+            <View
+              className="  flex-1 bg-slate-500 text-center justify-center  "
+              style={{
+                backgroundColor:
+                  item.isSelected === true ? "#ffff00" : "#D3D3D3",
+              }}
+            >
+              <Text className="text-center text-2xl font-bold ">
+                {item.name}
+              </Text>
+            </View>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+  let w = span.length * 4;
+  const classes =
+    `bg-slate-500` +
+    (w ? (w >= 12 ? ` w-full` : ` w-${w}/12`) : ` flex:1 w-0 `) +
+    ` h-2` +
+    ` transition-transform`;
+  return (
+    <SafeAreaView className="mt-4 mx-1">
+      <View className="mt-6 text-centre">
+        <Text className="font-bold text-center text-black text-3xl">
+          Follow any three!
+        </Text>
+        <Text className="font-bold text-center text-zinc text-base  ">
+          Then we'll build a custom homefeed for you
+        </Text>
+      </View>
+      {/* Render Component */}
+      <View>
+        <FlatList
+          numColumns={2}
+          data={data}
+          renderItem={renderComponent}
+          keyExtractor={(item) => item.id}
+          className="w-full h-f/5 p-3 flex-row flex-wrap"
+        />
+      </View>
+      <View style={{ flexDirection: "row", flex: 1 }}>
+        <View className={classes}></View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default Category;
